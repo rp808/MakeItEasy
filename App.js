7a9a6b4a1895f82fe3d9655a10429038bdbda5d4
Romi Patel <romipatel808@gmail.com>
2023-06-06 13:11:02 -0600

    Added style to the login component

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import Login from './src/Login';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <View style={styles.container}>
--- src/Login.js
+++ src/Login.js
@@ -1,3 +1,4 @@
+import { StatusBar } from "expo-status-bar";
 import React, {useState} from 'react';
 import {
     StyleSheet,
@@ -14,16 +15,85 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Login = () => {
   return (
-    <View>
-     <View>
-        <TextInput   placeholder="Email."></TextInput>
+    <View style={styles.screen}> 
+      <StatusBar style="auto" />
+
+     <View  style={styles.box}>
+        <TextInput  style={styles.textInput}
+        placeholder="Email."
+        ></TextInput>
      </View>
-     <View>
-     <TextInput   placeholder="Password."></TextInput>
+     <View style={styles.box}>
+     <TextInput  style={styles.textInput}
+       placeholder="Password."></TextInput>
      </View>
-     
+     <View style={styles.loginBtn}>
+        <Text>LOGIN</Text>
+     </View>
+     <View style={styles.hairline} />
+     <View style={styles.signUpTxt}> 
+     <Text >"Don't have an account?" Sign Up</Text>
+     </View>
+
      </View>
   )
 }
+const styles = StyleSheet.create({
+    
+    
+screen:{
+    flex:1,
+    width:'100%',
+    backgroundColor:"#fff",
+    alignItems:"center",
+    justifyContent:"center",
+},
+box:{
+   
+    borderRadius: 10,
+    borderWidth:1,
+    height:45,
+    width:'80%',
+    marginBottom: 20,
 
-export default Login
+},
+textInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+    width: "80%",
+  },
+loginBtn:{
+    width: "60%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
+    backgroundColor: "#8DAA6F",
+},
+hairline: {
+    marginTop: 40,
+    backgroundColor: '#A2A2A2',
+    height: 2,
+    width: '80%',
+  
+  },
+  signUpTxt:{
+    marginTop: 5,
+    height: 30,
+    marginBottom: 30,
+
+  },
+  
+//   loginButtonBelowText1: {
+//     fontFamily: 'AvenirNext-Bold',
+//     fontSize: 14,
+//     paddingHorizontal: 5,
+//     alignSelf: 'center',
+//     color: '#A2A2A2'
+//   },
+});
+
+export default Login;
