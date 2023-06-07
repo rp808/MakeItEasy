f024e1f83b3a3a2aed3361a9a1960ef29fdfffe2
Romi Patel <romipatel808@gmail.com>
2023-06-07 14:33:21 -0600

    modified style for title and rating, shadow added

--- src/Recipe.js
+++ src/Recipe.js
@@ -30,17 +30,27 @@ const Recipe = () => {
             <View style={styles.titleRating}>
 
                 <View style={styles.titleContainer}>
-                    <Text style={styles.recipeTitle}>Delicious Recipe</Text>
-                    <View style={styles.ratingContainer}>
-                        <Ionicons name="star" size={25} color="#000000" style={styles.starIcon} />
-                        <Text style={styles.ratingText}>4.5</Text>
+                    <View style={styles.backgroundContainer}>
+                        <Text style={styles.recipeTitle}>Delicious Recipe</Text>
+                        <View style={styles.ratingContainer}>
+                            <Ionicons name="star" size={25} color="#000000" style={styles.starIcon} />
+                            <Text style={styles.ratingText}>4.5</Text>
+                        </View>
                     </View>
                 </View>
-
             </View>
 
 
-            <View style={styles.ingreInstruct}></View>
+            <View style={styles.ingreInstruct}>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Ingredients</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Instructions</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
 
 
             <View style={styles.userRating}></View>
@@ -77,16 +87,19 @@ const styles = StyleSheet.create({
     titleRating: {
         flex: 0.5,
         width: '100%',
+        
         backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
+        // marginRight: 25,
+        // marginLeft:25,
+        // alignItems: "center",
+        // justifyContent: "center",
     },
     ingreInstruct: {
         flex: 3,
         width: '100%',
         backgroundColor: "#fbc",
-        alignItems: "center",
-        justifyContent: "center",
+        // alignItems: "center",
+        // justifyContent: "center",
     },
     userRating: {
         flex: 0.5,
@@ -110,28 +123,76 @@ const styles = StyleSheet.create({
         height: '100%',
     },
     titleContainer: {
+       width: '100%',
         flexDirection: 'row',
+        justifyContent: 'space-between',
+        // alignItems: 'center',
+         marginTop: 6,
+         paddingHorizontal: 16,
         alignItems: 'center',
-        marginTop: 16,
-      },
-      recipeTitle: {
+       // marginTop: -40,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+        elevation: 4,
+        
+        
+    },
+    backgroundContainer: {
+      //  margin:6,
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: 'rgba(255, 255, 255, 0.9)',
+        paddingHorizontal: 5,
+        paddingVertical: 10,
+        borderRadius: 8,
+        
+       
+    },
+    recipeTitle: {
         fontSize: 25,
-       // fontWeight: 'bold',
-       fontFamily:"GillSans-SemiBold",
-        marginRight: 8,
-        alignItems: 'left',
-      },
-      ratingContainer: {
+        // fontWeight: 'bold',
+        fontFamily: "GillSans-SemiBold",
+        marginLeft: 8,
+        // alignItems: 'left',
+        
+    },
+    ratingContainer: {
         flexDirection: 'row',
         alignItems: 'center',
-      },
-      starIcon: {
+        marginTop: 4,
+        
+
+    },
+    starIcon: {
         marginRight: 4,
-      },
-      ratingText: {
+    },
+    ratingText: {
         fontSize: 25,
-        fontFamily:"GillSans-SemiBold",
-      },
+        fontFamily: "GillSans-SemiBold",
+        marginRight: 35,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        marginTop: 16,
+    },
+    button: {
+        flex: 1,
+        alignItems: 'center',
+        paddingVertical: 8,
+        backgroundColor: '#E0E0E0',
+        marginHorizontal: 8,
+        borderRadius: 8,
+    },
+    buttonText: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
 });
 
 
