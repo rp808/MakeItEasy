6acb24eb0b665e1ac1b278be44638b70a47fd766
Romi Patel <romipatel808@gmail.com>
2023-06-06 20:17:22 -0600

    devide home screen into 3 parts using flex

--- src/Home.js
+++ src/Home.js
@@ -17,12 +17,24 @@ export const Home = () => {
     return(
     <View style={styles.screen}> 
     <StatusBar style="auto" />
+
+    <View style={styles.appNameFlex}>
+
+    </View>
+
+
+    <View style={styles.cardsFlex}>
+
+    </View>
+
+
+    <View style={styles.buttomNavFlex}>
+
+    </View>
  
    
   
-   <View style={styles.signUpTxt}> 
-   <Text >"Already have an account" Log In</Text>
-   </View>
+   
   </View>
     )
 };
@@ -36,14 +48,33 @@ const styles = StyleSheet.create({
         alignItems:"center",
         justifyContent:"center",
     },
+    appNameFlex:{
+        flex:1.25,
+        width:'100%',
+        backgroundColor:"#fbc",
+        alignItems:"center",
+        justifyContent:"center",
+    },
+    cardsFlex:{
+        flex:10,
+        width:'100%',
+        backgroundColor:"#fff",
+        alignItems:"center",
+        justifyContent:"center",
+    },
+    buttomNavFlex:{
+        flex:1,
+        width:'100%',
+        backgroundColor:"#fbc",
+        alignItems:"center",
+        justifyContent:"center",
+    },
+    //   signUpTxt:{
+    //     marginTop: 5,
+    //     height: 30,
+    //     marginBottom: 30,
     
-
-      signUpTxt:{
-        marginTop: 5,
-        height: 30,
-        marginBottom: 30,
-    
-      },
+    //   },
       
 
     });
