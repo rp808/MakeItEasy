9e8bc6c61bccdd84f67c52a293ec6b83f6a62352
Romi Patel <romipatel808@gmail.com>
2023-06-06 21:28:45 -0600

    added the logo/name of mobile app

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
     StyleSheet,
     Text,
@@ -9,72 +9,82 @@ import {
     Button,
     TouchableOpacity,
     Linking,
-  } from "react-native";
+} from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export const Home = () => {
-    return(
-    <View style={styles.screen}> 
-    <StatusBar style="auto" />
+    return (
+        <View style={styles.screen}>
+            <StatusBar style="auto" />
 
-    <View style={styles.appNameFlex}>
+            <View style={styles.appNameFlex}>
+                <View>
+                    <Text style={styles.logoTxt}>MakeItEasy</Text>
+                </View>
 
-    </View>
-
-
-    <View style={styles.cardsFlex}>
-
-    </View>
+            </View>
 
 
-    <View style={styles.buttomNavFlex}>
+            <View style={styles.cardsFlex}>
 
-    </View>
- 
-   
-  
-   
-  </View>
+            </View>
+
+
+            <View style={styles.buttomNavFlex}>
+
+            </View>
+
+
+
+
+        </View>
     )
 };
 const styles = StyleSheet.create({
-    
-    
-    screen:{
-        flex:1,
-        width:'100%',
-        backgroundColor:"#fff",
-        alignItems:"center",
-        justifyContent:"center",
+
+
+    screen: {
+        flex: 1,
+        width: '100%',
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
     },
-    appNameFlex:{
-        flex:1.25,
-        width:'100%',
-        backgroundColor:"#fbc",
-        alignItems:"center",
-        justifyContent:"center",
+    appNameFlex: {
+        flex: 1.2,
+        width: '100%',
+        backgroundColor: "#fbc",
+        paddingLeft:15,
+        alignItems: "start",
+        justifyContent: "center",
     },
-    cardsFlex:{
-        flex:10,
-        width:'100%',
-        backgroundColor:"#fff",
-        alignItems:"center",
-        justifyContent:"center",
+    cardsFlex: {
+        flex: 10,
+        width: '100%',
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
     },
-    buttomNavFlex:{
-        flex:1,
-        width:'100%',
-        backgroundColor:"#fbc",
-        alignItems:"center",
-        justifyContent:"center",
+    buttomNavFlex: {
+        flex: 1,
+        width: '100%',
+        backgroundColor: "#fbc",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    logoTxt:{
+        fontSize: 22,
+        fontWeight: 'bold',
+        paddingTop: 35,
+        fontFamily:'GillSans-SemiBoldItalic',
     },
     //   signUpTxt:{
     //     marginTop: 5,
     //     height: 30,
     //     marginBottom: 30,
-    
-    //   },
-      
 
-    });
+    //   },
+
+
+});
--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
     StyleSheet,
     Text,
@@ -9,121 +9,121 @@ import {
     Button,
     TouchableOpacity,
     Linking,
-  } from "react-native";
+} from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Login = () => {
-  return (
-    <View style={styles.screen}> 
-      <StatusBar style="auto" />
-    <View style={styles.boxFlex}>
-        <View>
-            <Text style={styles.welcomeTxt}>Welcome Back</Text>
-        </View>
-     <View  style={styles.box}>
-        <TextInput  style={styles.textInput}
-        placeholder="Email."
-        ></TextInput>
-     </View>
-   
-     
-     <View style={styles.box}>
-     <TextInput  style={styles.textInput}
-       placeholder="Password."></TextInput>
-     </View>
-     <View style={styles.loginBtn}>
-        <Text>LOGIN</Text>
-     </View>
-     </View>
-     
-    <View style={styles.bottomFlex}>
-     <View style={styles.hairline} />
-     <View style={styles.signUpTxt}> 
-     <Text >"Don't have an account?" Sign Up</Text>
-     </View>
-     </View>
-     </View>
+    return (
+        <View style={styles.screen}>
+            <StatusBar style="auto" />
+            <View style={styles.boxFlex}>
+                <View>
+                    <Text style={styles.welcomeTxt}>Welcome Back</Text>
+                </View>
+                <View style={styles.box}>
+                    <TextInput style={styles.textInput}
+                        placeholder="Email."
+                    ></TextInput>
+                </View>
 
-    
-  )
+
+                <View style={styles.box}>
+                    <TextInput style={styles.textInput}
+                        placeholder="Password."></TextInput>
+                </View>
+                <View style={styles.loginBtn}>
+                    <Text>LOGIN</Text>
+                </View>
+            </View>
+
+            <View style={styles.bottomFlex}>
+                <View style={styles.hairline} />
+                <View style={styles.signUpTxt}>
+                    <Text >"Don't have an account?" Sign Up</Text>
+                </View>
+            </View>
+        </View>
+
+
+    )
 }
 const styles = StyleSheet.create({
-    
-    
-screen:{
-    flex:1,
-    width:'100%',
-    backgroundColor:"#fff",
-    alignItems:"center",
-    justifyContent:"center",
-},
-boxFlex:{
-    flex:4,
-    width:'100%',
-    backgroundColor:"#fff",
-    alignItems:"center",
-    justifyContent:"flex-end",
-},
-bottomFlex:{
-    flex:2,
-    width:'100%',
-    backgroundColor:"#fff",
-    alignItems:"center",
-    justifyContent:"flex-end",
-    paddingBottom:30,
-},
-welcomeTxt:{
-    fontSize: 32,
-    fontWeight: 'bold',
-    paddingBottom:20,
-},
-box:{
-    
-    borderRadius: 10,
-    borderWidth:1,
-    height:45,
-    width:'80%',
-    marginBottom: 20,
-    
-},
-textInput: {
-    height: 50,
-    flex: 1,
-    padding: 10,
-    marginLeft: 20,
-    width: "80%",
-  },
-loginBtn:{
-    width: "60%",
-    borderRadius: 25,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 30,
-    backgroundColor: "#8DAA6F",
-},
-hairline: {
-    marginTop: 40,
-    backgroundColor: '#A2A2A2',
-    height: 2,
-    width: '80%',
-  
-  },
-  signUpTxt:{
-    marginTop: 5,
-    height: 30,
-    marginBottom: 30,
 
-  },
-  
-//   loginButtonBelowText1: {
-//     fontFamily: 'AvenirNext-Bold',
-//     fontSize: 14,
-//     paddingHorizontal: 5,
-//     alignSelf: 'center',
-//     color: '#A2A2A2'
-//   },
+
+    screen: {
+        flex: 1,
+        width: '100%',
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    boxFlex: {
+        flex: 4,
+        width: '100%',
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "flex-end",
+    },
+    bottomFlex: {
+        flex: 2,
+        width: '100%',
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "flex-end",
+        paddingBottom: 30,
+    },
+    welcomeTxt: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        paddingBottom: 20,
+    },
+    box: {
+
+        borderRadius: 10,
+        borderWidth: 1,
+        height: 45,
+        width: '80%',
+        marginBottom: 20,
+
+    },
+    textInput: {
+        height: 50,
+        flex: 1,
+        padding: 10,
+        marginLeft: 20,
+        width: "80%",
+    },
+    loginBtn: {
+        width: "60%",
+        borderRadius: 25,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 30,
+        backgroundColor: "#8DAA6F",
+    },
+    hairline: {
+        marginTop: 40,
+        backgroundColor: '#A2A2A2',
+        height: 2,
+        width: '80%',
+
+    },
+    signUpTxt: {
+        marginTop: 5,
+        height: 30,
+        marginBottom: 30,
+
+    },
+
+    //   loginButtonBelowText1: {
+    //     fontFamily: 'AvenirNext-Bold',
+    //     fontSize: 14,
+    //     paddingHorizontal: 5,
+    //     alignSelf: 'center',
+    //     color: '#A2A2A2'
+    //   },
 });
 
 export default Login;
