21d56d9df03a7b8e666ad697033aeb1ea6ab3635
Romi Patel <romipatel808@gmail.com>
2023-06-10 16:30:59 -0600

    adding backfround image and settled ingredients li

--- src/FilterPage.js
+++ src/FilterPage.js
@@ -10,7 +10,9 @@ import {
     TouchableOpacity,
     Linking,
     FlatList,
+    ImageBackground,
 } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -65,10 +67,36 @@ const FilterPage = () => {
         // Add more dietary restrictions here
     ];
 
+    const [ingredients, setIngredients] = useState([{ name: '', quantity: '' }]);
+
+    const handleIngredientChange = (index, field, value) => {
+        const updatedIngredients = [...ingredients];
+        updatedIngredients[index][field] = value;
+        setIngredients(updatedIngredients);
+    };
+
+    const addIngredientRow = () => {
+        setIngredients([...ingredients, { name: '', quantity: '' }]);
+    };
+
+    const handleSubmit = () => {
+        // Handle submission logic here
+        console.log(ingredients);
+    };
+
+    const handleReset = () => {
+        setIngredients([{ name: '', quantity: '' }]);
+    };
+
 
     return (
         (
             <View style={styles.screen}>
+                 <ImageBackground
+        source={require("./assets/back.jpg")} 
+        style={styles.imageBackground}
+        resizeMode="cover"
+      >
                 <StatusBar style="auto" />
 
                 <View style={styles.appNameFlex}>
@@ -117,7 +145,35 @@ const FilterPage = () => {
                 </View>
 
                 <View style={styles.avail}>
-
+                    <Text style={styles.label}>Available Ingredients</Text>
+                    {ingredients.map((ingredient, index) => (
+                        <View key={index} style={styles.ingredientRow}>
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Ingredient Name"
+                                value={ingredient.name}
+                                onChangeText={(value) => handleIngredientChange(index, 'name', value)}
+                            />
+                            <TextInput
+                                style={[styles.input, styles.quantityInput]}
+                                placeholder="Quantity (optional)"
+                                value={ingredient.quantity}
+                                onChangeText={(value) => handleIngredientChange(index, 'quantity', value)}
+                            />
+                        </View>
+                    ))}
+                    <TouchableOpacity style={styles.addButton} onPress={addIngredientRow}>
+                        <Ionicons name="add" size={24} color="white" />
+                        <Text style={styles.addButtonText}>Add Ingredient</Text>
+                    </TouchableOpacity>
+                    <View style={styles.buttonRow}>
+                        <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
+                            <Text style={styles.buttonText}>Submit</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.resetButton} onPress={handleReset}>
+                            <Text style={styles.buttonText}>Reset</Text>
+                        </TouchableOpacity>
+                    </View>
 
 
                 </View>
@@ -142,6 +198,7 @@ const FilterPage = () => {
 
 
 
+                </ImageBackground>
 
             </View>
         )
@@ -157,14 +214,18 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         width: '100%',
-        backgroundColor: '#FFF',
+        //backgroundColor: '#FFF',
         alignItems: "center",
         justifyContent: "center",
     },
+    imageBackground: {
+
+   
+      },
     appNameFlex: {
         flex: 0.8,
         width: '100%',
-        backgroundColor: "#fff",
+       // backgroundColor: "#fff",
         paddingLeft: 15,
         alignItems: "start",
         justifyContent: "center",
@@ -172,15 +233,15 @@ const styles = StyleSheet.create({
     avail: {
         flex: 4,
 
-        width: '100%',
-        backgroundColor: "#fbc",
-        alignItems: "right",
-        justifyContent: "center",
+     
+      //  backgroundColor: "#F9F9F9",
+        // alignItems: "right",
+        // justifyContent: "center",
 
     },
     levelThree: {
         flex: 1,
-        backgroundColor: "#F9F9F9",
+        //backgroundColor: "#F9F9F9",
         width: "100%",
 
         // alignItems: "right",
@@ -189,7 +250,7 @@ const styles = StyleSheet.create({
     diet: {
 
         flex: 1,
-        backgroundColor: "#F9F9F9",
+       // backgroundColor: "#F9F9F9",
         width: "100%",
 
 
@@ -207,15 +268,17 @@ const styles = StyleSheet.create({
         margin: 5,
     },
     radio: {
+        height:35,
         flexDirection: 'row',
         alignItems: 'center',
         marginRight: 10,
         marginBottom: 10,
-        marginLeft:13,
+        marginLeft: 5,
         padding: 5,
         borderRadius: 5,
         borderWidth: 1,
         borderColor: '#9A9A9A',
+        backgroundColor: '#FFF',
         marginTop: 10,
     },
     radioSelected: {
@@ -232,10 +295,11 @@ const styles = StyleSheet.create({
     label: {
         marginTop: 20,
         fontSize: 22,
-        marginLeft: "30%",
+        marginLeft: 5,
+        // marginLeft: "30%",
         // fontWeight: 'bold',GillSans-SemiBold
         fontFamily: "GillSans-SemiBold",
-        marginBottom: 5,
+        //marginBottom: 1,
     },
     checkboxContainer: {
         flexDirection: 'row',
@@ -253,6 +317,9 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: '#9A9A9A',
         marginTop: 10,
+        backgroundColor: '#FFF',
+        height:35,
+        //width:115,
     },
     checkboxSelected: {
         backgroundColor: '#AEAEAE',
@@ -262,13 +329,76 @@ const styles = StyleSheet.create({
         fontFamily: "GillSans-Light",
         fontSize: 18,
     },
-
+    // title: {
+    //     fontSize:  fontFamily: "GillSans-Light",16,
+    //     fontWeight: 'bold',
+    //     marginBottom: 10,
+    // },
+    ingredientRow: {
+        flexDirection: 'row',
+        marginBottom: 10,
+        marginLeft: 5,
+        marginTop:15,
+        
+    },
+    input: {
+        flex: 1,
+        marginRight: 10,
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        height:35,
+        backgroundColor: '#FFF',
+        fontFamily: "GillSans-SemiBold",
+    },
+    quantityInput: {
+        flex: 0.6,
+    },
+    addButton: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: 'black',
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        borderRadius: 5,
+        marginBottom: 10,
+        width: "40%",
+        marginLeft: 120,
+    },
+    addButtonText: {
+        color: 'white',
+        marginLeft: 5,
+        fontFamily: "GillSans-SemiBold",
+    },
+    buttonRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    submitButton: {
+        backgroundColor: 'black',
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        marginLeft: 5,
+        borderRadius: 5,
+    },
+    resetButton: {
+        backgroundColor: 'black',
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontFamily: "GillSans-SemiBold",
+    },
 
     buttomNavFlex: {
         flexDirection: 'row',
         justifyContent: "center",
         alignItems: 'center',
-        backgroundColor: '#fff',
+       // backgroundColor: '#fff',
         height: 74,
 
         flex: 1,
