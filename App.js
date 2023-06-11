c5cfed355c753250d408378144bb372ef3f04a27
Romi Patel <romipatel808@gmail.com>
2023-06-10 20:34:47 -0600

    REmoving back from login page

--- App.js
+++ App.js
@@ -19,7 +19,10 @@ export default function App() {
         <Stack.Screen
           name="Login"
           component={Login}  
-            />
+          options={
+            {
+            headerShown:false}
+          }  />
         <Stack.Screen name="Home" component={Home}      />
       </Stack.Navigator>
 
