8047e6da5de7fa8bd7dd00013d4988ff045e6302
Romi Patel <romipatel808@gmail.com>
2023-07-12 19:50:27 -0600

    adding user profile and save in bottom navigation in recipe filter page

--- src/RecipeFilter.js
+++ src/RecipeFilter.js
@@ -232,22 +232,24 @@ const RecipeFilter = ({props,token}) => {
 
             </View>
             <View style={styles.buttomNavFlex}>
-                <TouchableOpacity style={styles.iconContainer} onPress={() => navigation.navigate('Home')}>
-                    <Image style={styles.iconImg} source={require("./assets/homeNF.png")} />
-                    {/* <Text style={styles.ttitle}>Homee</Text> */}
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.iconContainer} onPress={() => navigation.navigate('FilterPage')}>
-                    <Image style={styles.iconImg} source={require("./assets/filter.png")} />
-                    {/* <Text style={styles.ttitle}>Homee</Text> */}
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.iconContainer} onPress={() => navigation.navigate('Login')}>
-                    <Image style={styles.iconImg} source={require("./assets/logout.png")} />
-                    {/* <Text style={styles.ttitle}>Homee</Text> */}
-                </TouchableOpacity>
+                    <View style={styles.iconContainer}>
+                        <Image style={styles.iconImg} source={require("./assets/homeNF.png")} />
 
+                    </View>
+                    <TouchableOpacity style={styles.iconContainer} onPress={() => navigation.navigate('FilterPage')}>
+                        <Image style={styles.iconImg} source={require("./assets/filter.png")} />
 
-            </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.iconContainer} onPress={() => navigation.navigate('SaveRecipe')}>
+                        <Image style={styles.iconImg} source={require("./assets/save.png")} />
 
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.iconContainer} onPress={() => navigation.navigate('UserProfile')}>
+                        <Image style={styles.iconImg} source={require("./assets/userFilled.png")} />
+
+                    </TouchableOpacity>
+
+                </View>
 
 
 
