9ddeb91ae628e5d080db824507a1e779f5bbc657
Romi Patel <romipatel808@gmail.com>
2023-07-22 16:16:08 -0600

    added remove functionality in save page

--- src/SaveRecipe.js
+++ src/SaveRecipe.js
@@ -34,6 +34,28 @@ const SaveRecipe = ({ navigation, token }) => {
             });
     };
 
+    const handleRemoveRecipe = (recipeId) => {
+        fetch(`${API_BASE_URL}/saved-recipes/remove`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            authorization: 'Bearer ' + token,
+          },
+          body: JSON.stringify({ recipeId }),
+        })
+          .then((response) => {
+            if (response.ok) {
+              console.log('Recipe removed successfully');
+              setSavedRecipes(savedRecipes.filter((recipe) => recipe.id !== recipeId));
+            } else {
+              throw new Error('Failed to remove the recipe');
+            }
+          })
+          .catch((error) => {
+            console.error('Error removing the recipe:', error);
+          });
+      };
+
     return (
         <View style={styles.container}>
             <View style={styles.appNameFlex}>
@@ -50,13 +72,13 @@ const SaveRecipe = ({ navigation, token }) => {
             <ScrollView style={styles.scrollContainer}>
                 {savedRecipes.map((recipe) => (
                     <TouchableOpacity key={recipe._id} style={styles.cardContainer}>
-                        <Image style={styles.image} source={{ uri: recipe.imageSource }}  />
+                        <Image style={styles.image} source={{ uri: recipe.imageSource }} />
                         <View style={styles.descCard}>
-                        <Text style={styles.title}>{recipe.description}</Text>
-                        <TouchableOpacity style={styles.saveC} onPress={() => handleSaveRecipe(card.id)}>
-                                <Image style={styles.saveImg} source={require("./assets/saveCard.png")} />
+                            <Text style={styles.title}>{recipe.description}</Text>
+                            <TouchableOpacity style={styles.saveC}   onPress={() => handleRemoveRecipe(recipe.id)} >
+                                <Image style={styles.saveImg} source={require("./assets/saveFilled.png")} />
                             </TouchableOpacity>
-                            </View>
+                        </View>
                         {/* 
             <Text>{recipe.instructions}</Text> */}
 
