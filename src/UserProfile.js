69b65d951ce87b3eef2d9386a1c17466924695af
Romi Patel <romipatel808@gmail.com>
2023-07-22 20:17:28 -0600

    added option to select dietary restriction in user profile page

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -15,34 +15,65 @@ import {
 } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
 
-const UserProfile = ({navigation , token}) => {
-//console.log("token",token);
+const UserProfile = ({ navigation, token }) => {
+    //console.log("token",token);
     const [userData, setUserData] = useState(null);
+    const [selectedItems, setSelectedItems] = useState([]);
+    const dietaryRestrictionsOptions = [
+        {
+            label: 'Vegetarian',
+            value: 'vegetarian',
+        },
+        {
+            label: 'Vegan',
+            value: 'vegan',
+        },
+        {
+            label: 'Gluten-free',
+            value: 'gluten-free',
+        },
+        {
+            label: 'Non-Veg',
+            value: 'non-vegetarian',
+        },
+        {
+            label: 'All',
+            value: 'all',
+        },
 
+    ];
+
+    const handleSelection = (item) => {
+        if (selectedItems.includes(item)) {
+            setSelectedItems(selectedItems.filter((selectedItem) => selectedItem !== item));
+        } else {
+            setSelectedItems([...selectedItems, item]);
+        }
+    };
     useEffect(() => {
         fetchUserProfile();
-      }, []);
-      const fetchUserProfile = async () => {
+    }, []);
+    const fetchUserProfile = async () => {
         try {
-          const response = await fetch(`${API_BASE_URL}/profile`, {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${token}`,
-              'Content-Type': 'application/json',
-            },
-          });
-    
-          const data = await response.json();
-    
-          if (response.ok) {
-            setUserData(data);
-          } else {
-            console.log('Error:', data.error);
-          }
+            const response = await fetch(`${API_BASE_URL}/profile`, {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                setUserData(data);
+            } else {
+                console.log('Error:', data.error);
+            }
         } catch (error) {
-          console.log('Error:', error.message);
+            console.log('Error:', error.message);
         }
-      };
+    };
 
     return (
         (
@@ -63,6 +94,27 @@ const UserProfile = ({navigation , token}) => {
                 <View style={styles.userDiet}>
                     <View>
                         <Text style={styles.logoTxt}>Dietary Restriction : user's choice</Text>
+                        <View style={styles.checkboxContainer}>
+                            {dietaryRestrictionsOptions.map((option) => (
+                                <TouchableOpacity
+                                    key={option.value}
+                                    style={[
+                                        styles.checkbox,
+                                        selectedItems.includes(option.value) && styles.checkboxSelected,
+                                    ]}
+                                    onPress={() => handleSelection(option.value)}
+                                >
+                                    <Text
+                                        style={[
+                                            styles.checkboxLabel,
+                                            selectedItems.includes(option.value) ? styles.selectedCheckboxLabel : null,
+                                        ]}
+                                    >
+                                        {option.label}
+                                    </Text>
+                                </TouchableOpacity>
+                            ))}
+                        </View>
                     </View>
                 </View>
 
@@ -115,7 +167,7 @@ const styles = StyleSheet.create({
         alignItems: "start",
         flex: 2,
     },
-    userDiet:{
+    userDiet: {
         alignItems: "start",
         flex: 6,
     },
@@ -153,6 +205,64 @@ const styles = StyleSheet.create({
         marginBottom: 10,
         marginTop: 10,
     },
+    checkboxLabel: {
+
+        color: 'black',
+
+    },
+    selectedCheckboxLabel: {
+
+        color: 'white',
+
+    },
+    label: {
+        fontSize: 22,
+        fontFamily: "GillSans-SemiBold",
+        marginBottom: 10,
+    },
+    checkboxContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        margin: 1,
+
+    },
+    checkbox: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginRight: 13,
+        marginBottom: 10,
+        padding: 3,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#9A9A9A',
+        marginTop: 10,
+        backgroundColor: '#FFF',
+        height: 35,
+    },
+
+    checkboxSelected: {
+        backgroundColor: '#05595b',
+        // color:'white',
+        transform: [{ scale: 1.1 }], // Scale up the selected checkbox
+        borderWidth: 0,
+        borderColor: '#000',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    checkboxLabel: {
+        marginLeft: 2,
+        fontFamily: "GillSans-Light",
+        fontSize: 18,
+    },
+
+
+
 
 });
 
